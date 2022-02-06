a185eee037c67cd0e14c4cab6871b66f94a00af1
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-02-06 14:35:34 +0100

    Creating new table for categories and adding relation to posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title'];
+    protected $guarded = [];
+    // protected $fillable = ['title', 'excerpt', 'body'];
 
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
 }
