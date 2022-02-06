8d18d9f79eb9ceb0904d1134ac60ef61f1270653
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-02-06 14:11:44 +0100

    Creating views and layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('/post/{post}', function ($id) {
