9996023bcc2182c6ad6a47da31a91000f3b97baa
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-02-11 11:13:09 +0100

    Working with database seeder and with relations between tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quidem recusandae eveniet, quos quasi quo obcaecati labore! Ex laboriosam at quasi consectetur maiores dolores esse quibusdam deserunt asperiores facilis. Molestias.'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quidem recusandae eveniet, quos quasi quo obcaecati labore! Ex laboriosam at quasi consectetur maiores dolores esse quibusdam deserunt asperiores facilis. Molestias.'
+        ]);
     }
 }
